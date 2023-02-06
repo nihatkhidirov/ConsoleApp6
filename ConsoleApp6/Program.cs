12ee164a9c23c0b269c85429d394803295d44466
hp <hp@DESKTOP-77U60MC>
2023-02-06 23:48:01 +0400

    Add project files.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,84 @@
+﻿namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                #region Task 2
+                //    Console.Write("Ededinizi Daxil Edin: ");
+                //    int number = int.Parse(Console.ReadLine());
+                //    int sum = 0; int count = 0;
+
+                //    count = number.ToString().Length;
+
+                //    for (int a = count; a > 0; a--)
+                //    {
+                //        int digit = number % 10;
+                //        sum = sum + (a * digit);
+                //        number /= 10;
+                //    }
+                //    Console.WriteLine("Reqemlerin cemi : " + sum);
+                #endregion
+
+
+
+                {
+                    #region Task 1
+
+                    //    Console.Write("Ededinizi Daxil edin : ");
+                    //    int number = int.Parse(Console.ReadLine());
+                    //    int sum = 0; int position = 1;
+
+                    //    while (number > 0)
+                    //    {
+                    //        int digitofnumbers = number % 10;
+                    //        if (position % 2 == 0)
+                    //        {
+                    //            sum = sum + digitofnumbers * digitofnumbers;
+                    //        }
+                    //        position++;
+                    //        number /= 10;
+                    //    }
+
+                    //    Console.WriteLine("Cut xanadaki ededlerin cemi: " + sum);
+
+                    #endregion
+                    //}
+
+                    #region Task 3
+
+                    //            Console.Write("Eded daxil edin: ");
+                    //            int number = int.Parse(Console.ReadLine());
+                    //           number = number/ 10; 
+                    //           int Digit1 = number % 10;
+
+
+                    //            while (number > 0)
+                    //            {
+                    //                int digit = number % 10;
+                    //                if (digit != firstDigit)
+                    //                {
+                    //                    Console.WriteLine("Eyni deyil");
+                    //                    return;
+                    //                }
+                    //                number = number/10;
+                    //            }
+
+                    //            Console.WriteLine("Eyni");
+
+                    #endregion
+                    //        }
+                    //    }
+                    //}
+
+
+                }
+
+            }
+
+        }
+
+    }
+}
+}
